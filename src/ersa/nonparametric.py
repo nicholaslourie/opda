Fix bug in EmpiricalDistribution.average_tuning_curve
Make EmpiricalDistribution.average_tuning_curve properly handle
distributions with infinite support. When the distribution puts
non-zero probability mass at negative or positive infinity, the
tuning curve's expected value is either negative infinity, positive
infinity, or undefined (NaN). Prior to this commit,
average_tuning_curve would just ignore negative and positive
infinity, causing some of the probability mass to be ignored and
leading to nonsensical outputs when the distribution had support
at infinity.

src/ersa/nonparametric.py
@@ -213,6 +213,7 @@ class EmpiricalDistribution:
             np.concatenate([[0.] * len(prepend), self.ws, [0.] * len(postpend)]),
         )
         self._ws_cumsum = np.cumsum(self._ws)
+        self._ws_cumsum_prev = np.concatenate([[0.], self._ws_cumsum[:-1]])
 
     def sample(self, size):
         """Return a sample from the empirical distribution.
@@ -361,11 +362,12 @@ class EmpiricalDistribution:
             raise ValueError('ns must be positive.')
 
         # Compute the average tuning curve.
+        ws = (
+            self._ws_cumsum**ns[..., None]
+            - self._ws_cumsum_prev**ns[..., None]
+        )
         return np.sum(
-            (
-                self._ws_cumsum[1:-1]**ns[..., None]
-                - self._ws_cumsum[:-2]**ns[..., None]
-            ) * self._ys[1:-1],
+            ws * np.where(ws != 0., self._ys, 0.),
             axis=-1,
         )
 