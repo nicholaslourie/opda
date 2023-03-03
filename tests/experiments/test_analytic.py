72baac04059ec1ce11c3a91c72a9e3080fe3f274
Nicholas Lourie <dev@nicholaslourie.com>
2023-03-02 21:27:26 -0500

    Add experiments.analytic.get_approximation_parameters

--- tests/experiments/test_analytic.py
+++ tests/experiments/test_analytic.py
@@ -2,9 +2,12 @@
 
 import unittest
 
+from autograd import numpy as npx
 import numpy as np
+import pytest
 
-from experiments import analytic
+from ersa import nonparametric, parametric, utils
+from experiments import analytic, simulation
 
 
 class EllipseVolumeTestCase(unittest.TestCase):
@@ -54,3 +57,79 @@ class EllipseVolumeTestCase(unittest.TestCase):
             analytic.ellipse_volume([2, 3, 5, 7]),
             210 * 1./2 * np.pi**2,
         )
+
+
+class GetApproximationParametersTestCase(unittest.TestCase):
+    """Test experiments.analytic.get_approximation_parameters."""
+
+    @pytest.mark.level(1)
+    def test_get_approximation_parameters(self):
+        # Quadratic Functions
+        for n_dims in [1, 2, 3]:
+            n_ball_vol = analytic.ellipse_volume([1] * n_dims)
+            a, b, c = analytic.get_approximation_parameters(
+                func=lambda xs: - npx.sum(xs**2, axis=-1),
+                bounds=[(-0.5, 0.5)] * n_dims,
+            )
+            self.assertEqual(
+                a,
+                # minimum value (normalizer)
+                - (1 / n_ball_vol)**(2 / n_dims),
+            )
+            self.assertEqual(
+                b,
+                # maximum value
+                0.0,
+            )
+            self.assertEqual(
+                c,
+                # dimension / 2
+                n_dims / 2,
+            )
+        # Non-Quadratic Functions
+        n_trials = 10_000
+        n_samples = 100
+        for n_dims in [1, 2, 3]:
+            s = simulation.Simulation.run(
+                n_trials=n_trials,
+                n_samples=n_samples,
+                n_dims=n_dims,
+                func=simulation.make_damped_linear_sin(
+                    scale=1,
+                    weights=[1] * n_dims,
+                    bias=0,
+                ),
+                bounds=[(0.0, 0.5)] * n_dims,
+            )
+            approximate_distribution = parametric.QuadraticDistribution(
+                *analytic.get_approximation_parameters(
+                    func=s.func,
+                    bounds=s.bounds,
+                )
+            )
+            empirical_distribution = nonparametric.EmpiricalDistribution(
+                s.yss[:, 0],
+            )
+            empirical_max_distribution = nonparametric.EmpiricalDistribution(
+                s.yss_cummax[:, n_samples - 1]
+            )
+
+            ns = np.arange(n_samples // 2, n_samples + 1)
+            grid = np.linspace(s.y_min, s.y_max, num=10_000)
+            #     Compare the approximation to the CDF of the max.
+            self.assertTrue(np.allclose(
+                approximate_distribution.cdf(grid)**n_samples,
+                empirical_max_distribution.cdf(grid),
+                atol=utils.dkw_epsilon(n_trials, 0.999999),
+            ))
+            #     Compare the approximation and the tail of the tuning curves.
+            self.assertTrue(np.allclose(
+                approximate_distribution.quantile_tuning_curve(ns),
+                empirical_distribution.quantile_tuning_curve(ns),
+                atol=0.025,
+            ))
+            self.assertTrue(np.allclose(
+                approximate_distribution.average_tuning_curve(ns),
+                empirical_distribution.average_tuning_curve(ns),
+                atol=0.025,
+            ))
