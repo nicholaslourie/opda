8329907e90bd941123383d03d14ba428011c5050
Nicholas Lourie <dev@nicholaslourie.com>
2023-12-01 03:36:50 -0500

    Raise an error if a test is marked with multiple levels
    It is unclear when to run a test marked with multiple levels. Thus,
    marking a test with multiple levels is an error.

--- tests/conftest.py
+++ tests/conftest.py
@@ -18,8 +18,16 @@ def pytest_configure(config):
 
 def pytest_runtest_setup(item):
     level = item.config.getoption('--level')
+
     item_levels = [mark.args[0] for mark in item.iter_markers(name='level')]
-    if item_levels and all(item_level > level for item_level in item_levels):
+    if len(item_levels) == 0:
+        item_level = None
+    elif len(item_levels) == 1:
+        item_level = item_levels[0]
+    else:
+        raise RuntimeError('The test was marked with multiple levels.')
+
+    if item_level is not None and item_level > level:
         pytest.skip(
             'The test\'s level is higher than the current level being run.'
         )
