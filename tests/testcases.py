c343f20fb6bc95ec7cae6eaf8dc60b19a33a4a5d
Nicholas Lourie <dev@nicholaslourie.com>
2023-12-11 23:47:08 -0500

    Set opda's global default random state in tests on RandomTestCase

--- tests/testcases.py
+++ tests/testcases.py
@@ -5,6 +5,8 @@ import unittest
 
 import numpy as np
 
+import opda.random
+
 logger = logging.getLogger(__name__)
 
 
@@ -12,9 +14,14 @@ class RandomTestCase(unittest.TestCase):
     """A base class for tests involving randomness."""
 
     def setUp(self):
+        # Create the random seed and generator.
         self.seed = np.random.SeedSequence().entropy
         self.generator = np.random.default_rng(self.seed)
 
+        # Use the random seed and generator globally.
+        opda.random.set_seed(self.generator)
+
+        # Log the random seed so that tests can be reproduced.
         logger.info(
             f"Running {self.id()} with random seed: {self.seed}.",
         )
