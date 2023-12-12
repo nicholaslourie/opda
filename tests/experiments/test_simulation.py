0ba5030ad6825e994ff09578975dcb78860d97db
Nicholas Lourie <dev@nicholaslourie.com>
2023-12-12 00:13:08 -0500

    Add a generator parameter to Simulation.run
    Simulation.run uses numpy's legacy API for generating random
    numbers. Migrate off that API by adding a generator parameter which
    creates a new generator using entropy from the OS when None (the
    default). In addition, use this new generator parameter to make the
    existing test of Simulation.run deterministic, and add a new test
    that the generator parameter actually makes the simulation
    deterministic.

--- tests/experiments/test_simulation.py
+++ tests/experiments/test_simulation.py
@@ -1,11 +1,14 @@
 """Tests for experiments.simulation"""
 
+import dataclasses
 import unittest
 
 import numpy as np
 import pytest
 
 from experiments import simulation
+
+from tests import testcases
 
 
 class MakeDampedLinearSinTestCase(unittest.TestCase):
@@ -39,7 +42,7 @@ class MakeDampedLinearSinTestCase(unittest.TestCase):
         )
 
 
-class SimulationTestCase(unittest.TestCase):
+class SimulationTestCase(testcases.RandomTestCase):
     """Test experiments.simulation.Simulation."""
 
     @pytest.mark.level(1)
@@ -55,7 +58,7 @@ class SimulationTestCase(unittest.TestCase):
             ),
             "bounds": [[-1., 1.]] * 3,
         }
-        sim = simulation.Simulation.run(**sim_kwargs)
+        sim = simulation.Simulation.run(**sim_kwargs, generator=self.generator)
 
         for key, expected in sim_kwargs.items():
             actual = getattr(sim, key)
@@ -80,3 +83,50 @@ class SimulationTestCase(unittest.TestCase):
             sim.yss_cummax.shape,
             (sim.n_trials, sim.n_samples),
         )
+
+    @pytest.mark.level(1)
+    def test_run_is_deterministic_given_generator_argument(self):
+        sim_kwargs = {
+            "n_trials": 7,
+            "n_samples": 5,
+            "n_dims": 3,
+            "func": simulation.make_damped_linear_sin(
+                weights=[1., -1., 0.],
+                bias=0.,
+                scale=1.,
+            ),
+            "bounds": [[-1., 1.]] * 3,
+        }
+        def simulations_are_equal(s0, s1):
+            return all(
+                v0 == v1
+                if not isinstance(v0, np.ndarray) else
+                np.array_equal(v0, v1)
+                for v0, v1 in zip(
+                        dataclasses.astuple(s0),
+                        dataclasses.astuple(s1),
+                )
+            )
+
+        # Without a generator, two runs should be unequal.
+        self.assertFalse(simulations_are_equal(
+            simulation.Simulation.run(**sim_kwargs),
+            simulation.Simulation.run(**sim_kwargs),
+        ))
+        # Reusing the same generator, two runs should be unequal.
+        generator = np.random.default_rng(0)
+        self.assertFalse(simulations_are_equal(
+            simulation.Simulation.run(**sim_kwargs, generator=generator),
+            simulation.Simulation.run(**sim_kwargs, generator=generator),
+        ))
+        # Using generators in the same state, two runs should be equal.
+        self.assertTrue(simulations_are_equal(
+            simulation.Simulation.run(
+                **sim_kwargs,
+                generator=np.random.default_rng(0),
+            ),
+            simulation.Simulation.run(
+                **sim_kwargs,
+                generator=np.random.default_rng(0),
+            ),
+        ))
