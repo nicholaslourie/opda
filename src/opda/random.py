e83cb69cadd85dc8f2b374f44c8a8929acdfbc74
Nicholas Lourie <dev@nicholaslourie.com>
2023-12-19 15:53:44 -0800

    Fix cross-references to numpy objects
    Cross-references to numpy objects must use the full package name
    (i.e., `numpy` rather than `np`) in order for the reStructuredText
    cross-references to work. In addition, `numpy.random.Generator` is
    a class and cannot be cross-referenced as a function.

--- src/opda/random.py
+++ src/opda/random.py
@@ -15,8 +15,8 @@ To set the global random state, use the :py:func:`set_seed` function:
 Functions and methods using randomness also provide a ``generator``
 keyword argument. If ``generator`` is ``None`` then the global random
 state, ``DEFAULT_GENERATOR``, is used. Otherwise, the ``generator``
-argument should be an instance of :py:class:`np.random.Generator` and
-the function or method will use that for generating random values.
+argument should be an instance of :py:class:`numpy.random.Generator`
+and the function or method will use that for generating random values.
 
 If you require *local* control of random behavior (e.g., making a
 single function deterministic), pass the generator explicitly to the
@@ -93,9 +93,9 @@ def set_seed(seed):
         state. If ``None``, then the random seed is set using entropy
         from the operating system. If an integer, then that integer is
         used as a random seed. If an instance of
-        :py:func:`np.random.Generator`, then that generator is used as
-        the global default random number generator. See the ``seed``
-        parameter of :py:func:`np.random.default_rng` for details.
+        :py:class:`numpy.random.Generator`, then that generator is used
+        as the global default random number generator. See the ``seed``
+        parameter of :py:func:`numpy.random.default_rng` for details.
 
     Returns
     -------
