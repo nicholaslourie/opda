e6bcfba9399efdeefe617d3bcba8574a2ec50d61
Nicholas Lourie <dev@nicholaslourie.com>
2024-01-22 23:13:07 -0500

    Add opda.exceptions.IntegrationError

--- tests/opda/test_exceptions.py
+++ tests/opda/test_exceptions.py
@@ -5,6 +5,10 @@ import unittest
 from opda import exceptions  # noqa: F401
 
 
+class IntegrationErrorTestCase(unittest.TestCase):
+    """Test opda.exceptions.IntegrationError."""
+
+
 class NumericalErrorTestCase(unittest.TestCase):
     """Test opda.exceptions.NumericalError."""
 
