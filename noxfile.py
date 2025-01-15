676f3b4040d3843391497e6e1c13d90590879234
Nicholas Lourie <dev@nicholaslourie.com>
2025-01-15 11:19:30 -0500

    Add support for numpy 2.2

--- noxfile.py
+++ noxfile.py
@@ -35,7 +35,7 @@ SUPPORTED_PYTHON_VERSIONS = {"3.8", "3.9", "3.10", "3.11", "3.12", "3.13"}
 """All Python versions currently supported by opda."""
 
 SUPPORTED_PACKAGE_VERSIONS = {
-    "numpy": {"1.23", "1.24", "1.25", "1.26", "2.0", "2.1"},
+    "numpy": {"1.23", "1.24", "1.25", "1.26", "2.0", "2.1", "2.2"},
     "scipy": {"1.10", "1.11", "1.12", "1.13", "1.14"},
 }
 """All core package versions currently supported by opda."""
