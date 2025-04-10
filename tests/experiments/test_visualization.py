bd87131b1e189cb3de71998a01e92564917b982b
Nicholas Lourie <dev@nicholaslourie.com>
2025-04-10 12:20:59 -0400

    Inline plot_random_search
    The plot_random_search function is only used in one place. Instead
    of maintaining a function for this visualization, just inline it.

--- tests/experiments/test_visualization.py
+++ tests/experiments/test_visualization.py
@@ -5,10 +5,6 @@ import unittest
 from experiments import visualization
 
 
-class PlotRandomSearchTestCase(unittest.TestCase):
-    """Test experiments.visualization.plot_random_search."""
-
-
 class PlotCdfTestCase(unittest.TestCase):
     """Test experiments.visualization.plot_cdf."""
 
